7c4963b9de410eaa13ec0eec8afd636bdd57801a
kehinde ogunyade <ogunyade.kehinde@gmail.com>
2021-08-29 07:47:57 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
     alert('kehinde billions')
   }
   return (
-    <div className="App" style={{maxWidth:'500px', margin:'30px auto'}}>
+    <div className="App" style={{maxWidth:'500px', margin:'30px auto', marginColor:'black'}}>
       <Profile fullName ='kehindebillions' bio ='i am the richest man in Africa, i started my company with 1 naira ten years ago.' profession='software developer'>
         <img src='./mypicture.jpg' width="400px" heigth='400px' alt='mypicture'/> 
       </Profile>
 
-      <handleName><button onClick={handleName}>name</button></handleName>
+      <handleName ><button style={{backgroundColor:'red', width:'200px'}} onClick={handleName}>name</button></handleName>
     </div>
   );
 }
